e246cc44431e4517884d1f7d89014b5339308723
iftekhar <freelanceriftekhar@gmail.com>
2018-03-12 11:07:49 +0600

    upload v1.0.1

--- front-page.php
+++ front-page.php
@@ -12,36 +12,8 @@ get_header();
 	<section class="about_myself">
 		<h3 id="about">About Myself</h3>
 
-		<div class="icon-lookup">	
-
-			<div class="face-img"><span class="glyphicon glyphicon-pencil"></span></div>
-
-			<div class="icon-pencil"><span class="	glyphicon glyphicon-user"></span></div>
-			<div class="icon-mouse"><span class="glyphicon glyphicon-pencil"></span></div>
-			
-			<div class="icon-browser"><span class="glyphicon glyphicon-pencil"></span></div>
-			<div class="icon-video"><i class="fas fa-video"></i></span></div>
-
-		</div>
-		<div class="glarb">
-			<p><strong>This video is so good! </strong>Web design is a pretty new topic/area I'm trying to get a handle on. I was really intimidated when I started (honestly been intimidated for years). Your videos have been so helpful! I know this video is a few years old now but I hope you aren't still selling yourself short as a teacher. You're excellent. Thanks!</p>
-		</div>
+		
 	</section>	
-	<div style="background-color: #f00;float: left"><strong>This video is so good! </strong>Web design is a pretty new topic/area I'm trying to get a handle on. I was really intimidated when I started (honestly been intimidated for years). Your videos have been so helpful! I know this video is a few years old now but I hope you aren't still selling yourself short as a teacher. You're excellent. Thanks!</div>
-	<div style="background-color: #0f0"><strong>This video is so good! </strong>Web design is a pretty new topic/area I'm trying to get a handle on. I was really intimidated when I started (honestly been intimidated for years). Your videos have been so helpful! I know this video is a few years old now but I hope you aren't still selling yourself short as a teacher. You're excellent. Thanks!</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </div><!--container-fluid-->
 <?php 
